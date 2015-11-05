67aff3fb95ff114122ea364c10f9a6cc52971c8c
Emily Carson <emilyjanecarson@gmail.com>
2015-11-05 18:02:07 +0000

    added empty test framework and main class

---
+++ RockPaperScissorsTechTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RockPaperScissorsTechTest
+{
+	class MainClass
+	{
+		public static void RockPaperScissors (string player1, string player2)
+		{
+			if (player1 == "computer") {
+				randomChoice ();
+			} else {
+				setChoice(
+			}
+			if (player2 == "computer") {
+				
+			}
+
+		}
+	}
+}
