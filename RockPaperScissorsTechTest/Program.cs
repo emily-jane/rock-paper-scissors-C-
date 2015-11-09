a21b9e08441b6d146e98678b3bb138acd3eb4eb3
Emily Carson <emilyjanecarson@gmail.com>
2015-11-09 19:13:39 +0000

    created rockpaperscissors logic. one test failing. need to include nunit to references

--- RockPaperScissorsTechTest/Program.cs
+++ RockPaperScissorsTechTest/Program.cs
@@ -9,15 +9,41 @@ namespace RockPaperScissorsTechTest
 	{
 		public static void RockPaperScissors (string player1, string player2)
 		{
-			if (player1 == "computer") {
-				randomChoice ();
-			} else {
-				setChoice(
-			}
-			if (player2 == "computer") {
-				
-			}
+			if (!(new[] { "rock", "paper", "scissors" }.Contains(player1.ToLower ())) || !(new[] { "rock", "paper", "scissors" }.Contains(player2.ToLower ()))) {
+				return "You must enter rock, paper or scissors.";
+			};
 
+			if (player1 == player2) 
+			{
+				return "It is a tie!";
+			};
+
+			if (player1 == "rock") 
+			{
+				if (player2 == "paper") {
+					return "Player 2 wins!";
+				} else if (player2 == "scissors") {
+					return "Player 1 wins!";
+				}
+			};
+
+			if (player1 == "paper") 
+			{
+				if (player2 == "rock") {
+					return "Player 1 wins!";
+				} else if (player2 == "scissors") {
+					return "Player 2 wins!";
+				}
+			};
+
+			if (player1 == "scissors") 
+			{
+				if (player2 == "rock") {
+					return "Player 2 wins!";
+				} else if (player2 == "paper") {
+					return "Player 1 wins!";
+				}
+			};
 		}
 	}
 }
--- RockPaperScissorsTechTest/Test.cs
+++ RockPaperScissorsTechTest/Test.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
 
 namespace RockPaperScissorsTechTest
 {
-	public class Test
+	[TestFixture]
+	public class Tests
 	{
-		public Test ()
+		[Test]
+		public void ShouldReturnOneWinsRockvsScissors()
 		{
+			var main = new MainClass();
+			var expected = "Player 1 wins!";
+
+			var actual = main.RockPaperScissors("rock", "scissors");
+
+			CollectionAssert.AreEqual(expected, actual);
 		}
 	}
 }
