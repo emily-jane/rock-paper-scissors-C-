9bffc78a741c4d7bf05e3c1e171a36763a317117
Emily Carson <emilyjanecarson@gmail.com>
2015-11-10 17:11:26 +0000

    added unit reference package

--- RockPaperScissorsTechTest/Program.cs
+++ RockPaperScissorsTechTest/Program.cs
@@ -7,43 +7,35 @@ namespace RockPaperScissorsTechTest
 {
 	class MainClass
 	{
-		public static void RockPaperScissors (string player1, string player2)
+		public static string RockPaperScissors (string player1, string player2)
 		{
-			if (!(new[] { "rock", "paper", "scissors" }.Contains(player1.ToLower ())) || !(new[] { "rock", "paper", "scissors" }.Contains(player2.ToLower ()))) {
+			if (!(new[] { "rock", "paper", "scissors" }.Contains (player1.ToLower ())) || !(new[] { "rock", "paper", "scissors" }.Contains (player2.ToLower ()))) {
 				return "You must enter rock, paper or scissors.";
 			};
-
-			if (player1 == player2) 
-			{
+			if (player1 == player2) {
 				return "It is a tie!";
-			};
-
-			if (player1 == "rock") 
-			{
+			}
+			if (player1 == "rock") {
 				if (player2 == "paper") {
 					return "Player 2 wins!";
 				} else if (player2 == "scissors") {
 					return "Player 1 wins!";
 				}
-			};
-
-			if (player1 == "paper") 
-			{
+				;
+			} else if (player1 == "paper") {
 				if (player2 == "rock") {
 					return "Player 1 wins!";
 				} else if (player2 == "scissors") {
 					return "Player 2 wins!";
 				}
-			};
-
-			if (player1 == "scissors") 
-			{
+				;
+			} else if (player1 == "scissors") {
 				if (player2 == "rock") {
 					return "Player 2 wins!";
 				} else if (player2 == "paper") {
 					return "Player 1 wins!";
-				}
-			};
+				} 
+			}
 		}
 	}
 }
